4d8cb5134452f6d564c6656592697f30e03089c1
skudriashev <stas.kudriashev@gmail.com>
2020-03-28 17:49:09 -0700

    Add colored_time tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,6 +1,9 @@
 from parameterized import parameterized
 
-from pytest_timer.plugin import _get_result_color
+from pytest_timer.plugin import (
+    _colored_time,
+    _get_result_color,
+)
 
 
 class TestPlugin:
@@ -13,5 +16,12 @@ class TestPlugin:
     def test_get_result_color(self, time_taken, color):
         assert _get_result_color(time_taken) == color
 
-    def test_colored_time(self):
-        pass
+    @parameterized.expand([
+        (1.000, False, '\x1b[32m1.0000s\x1b[0m'),
+        (1.001, False, '\x1b[33m1.0010s\x1b[0m'),
+        (3.000, False, '\x1b[33m3.0000s\x1b[0m'),
+        (3.001, False, '\x1b[31m3.0010s\x1b[0m'),
+        (3.001, True, '3.0010s'),
+    ])
+    def test_colored_time(self, time_taken, timer_no_color, expected_result):
+        assert _colored_time(time_taken, timer_no_color) == expected_result
